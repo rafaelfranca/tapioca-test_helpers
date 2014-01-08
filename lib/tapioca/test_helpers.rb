8c63fd11f7f2fe00758d743fa125bbad622abeab
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-01-08 18:53:11 -0200

    Implement disable_animations

--- lib/tapioca/test_helpers.rb
+++ lib/tapioca/test_helpers.rb
@@ -1,7 +1,27 @@
 require "tapioca/test_helpers/version"
+require "action_view"
+require "action_view/helpers/javascript_helper"
 
 module Tapioca
   module TestHelpers
-    # Your code goes here...
+    # Public: generates javascript code to disable all animations so that tests
+    # won't timeout waiting for animations to run.
+    #
+    # - Turns off jQuery.fx.
+    # - Turns off bootstrap transition support.
+    # - Removes the fade class from modal boxes since that uses css animations.
+    # - Overrides collapsible elements to avoid waiting for the height css transition.
+    def disable_animations
+      javascript_tag <<-JAVASCRIPT
+        $.fx.off = true;
+        $.support.transition = false;
+
+        var disableBootstrapTransitions = function() {
+          $('.modal').removeClass('fade');
+          $('.collapse').css({ 'transition': 'height 0.005s', '-webkit-transition': 'height 0.005s' });
+        };
+        $(disableBootstrapTransitions); $(document).ajaxComplete(disableBootstrapTransitions);
+      JAVASCRIPT
+    end
   end
 end
